4bd4893743afcd2a093ff0bfe4578ed5ef63640f
tylernorth <tylernorth18@gmail.com>
2014-06-18 19:25:16 -0700

    Fixed boot from volume

--- bootable_image.py
+++ bootable_image.py
@@ -31,7 +31,7 @@ def wait_for_condition(check_function, item_id, whitelist, blacklist,
         if obj.status in blacklist:
             return None
         obj = check_function(item_id)
-        time.sleep(5)
+        time.sleep(60)
     return obj.id
 
 def get_glance_client(args):
@@ -57,13 +57,14 @@ def parse_args():
     a.add_argument('--key-name', help='Name of key to use')
     a.add_argument('--keep-image', action='store_true',
                    help='Keep injected keypair image')
+    a.add_argument('--volume-type', default='ceph', help='Volume type')
     a.add_argument('--os-username', help='OpenStack Auth username')
     a.add_argument('--os-password', help='OpenStack Auth password')
     a.add_argument('--os-tenant-name', help='OpenStack Auth tenant name')
     a.add_argument('--os-auth-url', help='OpenStack Auth keystone url')
     return a.parse_args()
 
-def get_args(args):
+def get_env_args(args):
     if args['os_username'] == None and 'OS_USERNAME' in os.environ.keys():
         args['os_username'] = os.environ['OS_USERNAME']
     if args['os_password'] == None and 'OS_PASSWORD' in os.environ.keys():
@@ -74,14 +75,11 @@ def get_args(args):
         args['os_auth_url'] = os.environ['OS_AUTH_URL']
     return args
 
-def create_keypair_image(args):
-    nova = nova_v1.Client(args['os_username'], args['os_password'],
-                          args['os_tenant_name'], args['os_auth_url'])
-    glance = get_glance_client(args)
+def create_keypair_image(nova, glance, image_id, key_name):
     server_name = random_string(prefix='dummy-')
     log.info('Creating dummy server')
-    server = nova.servers.create(server_name, args['image-id'],
-                                 tiny_flavor, key_name=args['key_name'])
+    server = nova.servers.create(server_name, image_id,
+                                 tiny_flavor, key_name=key_name)
     log.info('Server created:%s' % server.id)
     log.info('Waiting for server:%s' % server.id)
     result = wait_for_condition(nova.servers.get, server.id,
@@ -104,71 +102,63 @@ def create_keypair_image(args):
     log.info('Deleting dummy server:%s' % server.id)
     nova.servers.delete(server.id)
     image = glance.images.get(image_id)
-    args['image-id'] = image.id
-    print args
-    if not args['keep_image']:
-        args['delete-image'] = True
-    return args
+    return image
 
-def boot_server(args):
-    glance = get_glance_client(args)
-    image = glance.images.get(args['image-id'])
-    cinder = cinder_v1.Client(args['os_username'], args['os_password'],
-                              args['os_tenant_name'], args['os_auth_url'])
-    nova = nova_v1.Client(args['os_username'], args['os_password'],
-                          args['os_tenant_name'], args['os_auth_url'])
+def shrink_image(cinder, nova, glance, image):
+    real_size = image.size / (1024*1024*1024.0)
+    log.info('Image:%s not large enough, shrinking' % image.id)
+    vol = cinder.volumes.create(real_size + 1, imageRef=image.id)
+    log.info('Volume created:%s, waiting' % vol.id)
+    result = wait_for_condition(cinder.volumes.get, vol.id,
+                                ['available'], ['error'])
+    log.info('Volume:%s created, converted back into image' % vol.id)
+    if result == None:
+        sys.exit('Error creating volume, exiting :(')
+    log.info('Deleting old dummy image:%s' % image.id)
+    glance.images.delete(image.id)
+    image_name = random_string(prefix='dummy')
+    log.info('Creating shurnk image from volume:%s' % vol.id)
+    image = cinder.volumes.upload_to_image(vol.id, True,
+                                           image_name, 'bare', 'qcow2')
+    image_id = image[1]['os-volume_upload_image']['image_id']
+    log.info('Waiting for new image:%s' % image_id)
+    result = wait_for_condition(nova.images.get, image_id,
+                                ['ACTIVE'], ['ERROR'])
+    if result == None:
+        sys.exit('Error creating volume, exiting :(')
+    log.info('New image created:%s, checking size' % image_id)
+    image = glance.images.get(image_id)
+    real_size = image.size / (1024*1024*1024.0)
+    log.info('Creating dummy volume:%s' % vol.id)
+    cinder.volumes.delete(vol.id)
+    return image
+
+def boot_server(glance, cinder, nova,
+                image_id, flavor_id, vol_size, server_name,
+                volume_type):
+    image = glance.images.get(image_id)
     real_size = image.size /(1024*1024*1024.0)
-    if args['vol-size'] < real_size:
-        log.info('Image:%s not large enough, shrinking' % image.id)
-        vol = cinder.volumes.create(real_size + 1, imageRef=image.id)
-        log.info('Volume created:%s, waiting' % vol.id)
-        result = wait_for_condition(cinder.volumes.get, vol.id,
-                                    ['available'], ['error'])
-        log.info('Volume:%s created, converted back into image' % vol.id)
-        if result == None:
-            sys.exit('Error creating volume, exiting :(')
-        log.info('Deleting old dummy image:%s' % image.id)
-        image_name = random_string(prefix='dummy')
-        log.info('Creating shurnk image from volume:%s' % vol.id)
-        image = cinder.volumes.upload_to_image(vol.id, True,
-                                               image_name, 'bare', 'qcow2')
-        image_id = image[1]['os-volume_upload_image']['image_id']
-        log.info('Waiting for new image:%s' % image_id)
-        result = wait_for_condition(nova.images.get, image_id,
-                                    ['ACTIVE'], ['ERROR'])
-        if result == None:
-            sys.exit('Error creating volume, exiting :(')
-        log.info('New image created:%s, checking size' % image_id)
-        image = glance.images.get(image_id)
+    if vol_size < real_size:
+        image = shrink_image(cinder, nova, glance, image)
         real_size = image.size / (1024*1024*1024.0)
-        log.info('Creating dummy volume:%s' % vol.id)
-        cinder.volumes.delete(vol.id)
-        if args['vol-size'] < real_size:
+        if vol_size < real_size:
             sys.exit('Your volume size isnt big enough, need:%s GB' % real_size)
-        args['image-id'] = image.id
-    nova = nova_v1.Client(args['os_username'], args['os_password'],
-                          args['os_tenant_name'], args['os_auth_url'])
-    cinder = cinder_v1.Client(args['os_username'], args['os_password'],
-                              args['os_tenant_name'], args['os_auth_url'])
+        image_id = image.id
     log.info('Creating bootable volume')
-    vol = cinder.volumes.create(args['vol-size'], imageRef=args['image-id'],
-                                volume_type='ceph')
+    vol = cinder.volumes.create(vol_size, imageRef=image_id,
+                                volume_type=volume_type)
     log.info("Volume created:%s, waiting" % vol.id)
     result = wait_for_condition(cinder.volumes.get, vol.id,
                                 ['available'], ['error'])
     if result == None:
         sys.exit('Your volume cant boot, exiting :(')
     log.info('Volume avaialable:%s, booting server' % vol.id)
-    name = args['name']
-    if name == None:
+    if server_name == None:
         name = random_string(prefix='server-')
     block_mapping = {'vda' : '%s:::0' % str(vol.id)}
-    server = nova.servers.create(name, args['image-id'], args['flavor-id'],
+    server = nova.servers.create(name, image_id, flavor_id,
                                  block_device_mapping=block_mapping)
     log.info('Server:%s created, waiting' % server.id)
-    if args['delete-image']:
-        glance = get_glance_client(args)
-        glance.images.delete(args['image-id'])
     result = wait_for_condition(nova.servers.get, server.id,
                                 ['ACTIVE'], ['ERROR'])
     if result == None:
@@ -178,11 +168,24 @@ def boot_server(args):
 
 def main():
     args = vars(parse_args())
-    args = get_args(args)
-    args['delete-image'] = True
-    if args['key_name'] != None:
-        args = create_keypair_image(args)
-    boot_server(args)
+    args = get_env_args(args)
+    glance = get_glance_client(args)
+    cinder = cinder_v1.Client(args['os_username'], args['os_password'],
+                              args['os_tenant_name'], args['os_auth_url'])
+    nova = nova_v1.Client(args['os_username'], args['os_password'],
+                          args['os_tenant_name'], args['os_auth_url'])
 
+    delete_image = False
+    if args['key_name'] != None:
+        image = create_keypair_image(nova, glance, args['image-id'],
+                                     args['key_name'])
+        args['image-id'] = image.id
+        delete_image = True
+        if args['keep_image']:
+            delete_image = False
+    boot_server(glance, cinder, nova, args['image-id'], args['flavor-id'],
+                args['vol-size'], args['name'], args['volume_type'])
+    if delete_image:
+        glance.images.delete(args['image-id'])
 if __name__ == '__main__':
     main()
