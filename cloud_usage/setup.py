9d1d6cb06d33e74bb16cfa8b669f6b6cf8c9c7cf
tylernorth <tylernorth18@gmail.com>
2014-08-13 17:11:10 -0700

    Add setup.py

---
+++ cloud_usage/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import setuptools
+
+VERSION = 0.1
+
+setuptools.setup(
+    author='Tyler Daniel North',
+    author_email='tylernorth18@gmail.com',
+    description='OpenStack Cloud Usage Script',
+    install_requires=[
+        'python-cinderclient >= 1.0.9',
+        'python-glanceclient >= 0.13.1',
+        'python-keystoneclient >= 0.10.1',
+        'python-novaclient >= 2.18.1',
+        'python-swiftclient >= 2.2.0',
+    ],
+    entry_points={
+        'console_scripts' : [
+            'cloud-usage = cloud_usage.cli:main',
+        ]
+    },
+    packages=[
+        'cloud_usage'
+    ],
+    name='cloud_usage',
+    version=VERSION,
+)
